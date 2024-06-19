dd661d0b89771610d1294ef92c00a28c24426471
jpenagos32 <jpenagosdev@gmail.com>
2024-06-19 17:48:31 -0500

    Agregar repaso aula 4 y archivos aula 5

---
+++ 2034-logica-programacion-1-Aula4/Repaso/app.js
@@ -0,0 +1,68 @@
+// ? Ejercicio 1
+console.log('Bienvenid@ al programa')
+
+// ? ejercicio 2
+// const nombre = 'Julian';
+// console.log(`Hola ${nombre}`);
+
+// ? Ejercicio 3
+// alert(`Hola ${nombre}`);
+
+// ? Ejercicio 4
+// const languaje = prompt('Cual es el lenguaje de programación que más te gusta?');
+
+// console.log('Tu lengueja de programación favorito es:', languaje)
+
+// ? Ejercicio 5
+// let valor1 = 10;
+// let valor2 = 15;
+// let resultado = valor1 + valor2;
+// console.log(`La suma de ${valor1} y ${valor2} es ${resultado}`);
+
+// ? Ejercicio 6
+// resultado = valor1 - valor2;
+// console.log(`La diferencia entre ${valor1} y ${valor2} es igual a ${resultado}`);
+
+// ? Ejercicio 7
+// const edadUsuario = prompt('Ingrese su edad');
+// if (edadUsuario <= 18) {
+//   console.log('Usted es menor de edad');
+// } else {
+//   console.log('Usted es mayor de edad');
+// }
+
+// ? Ejercicio 8
+// let numero = prompt('Porfavor ingrese un número');
+
+// if (numero > 0) {
+//   console.log(`El numero ${numero} es mayor a 0`);
+// } else if (numero < 0) {
+//   console.log(`El número ${numero} es menor a 0`)
+// } else {
+//   console.log('El número es 0')
+// }
+
+// ? Ejercicio 9
+// let incremento = 1
+
+// while (incremento <= 10) {
+//   console.log(incremento)
+//   incremento++
+// }
+
+// ? Ejercicio 10
+// let nota = 12;
+// if (nota >= 7) {
+//   console.log('Aprovado');
+// } else {
+//   console.log('Reprobado');
+// }
+
+// ? Ejercicio 11
+// console.log(Math.random())
+
+// ? Ejercicio 12
+// console.log(Math.floor(Math.random() * 10) + 1)
+
+// ? Ejercicio 13
+console.log(Math.floor(Math.random() * 1000) + 1)

07d3f75ab7d9a644c3e5acdc0d7d9a78163f1e54
jpenagos32 <jpenagosdev@gmail.com>
2024-06-19 15:54:03 -0500

    first commit

---
+++ 2034-logica-programacion-1-Aula1/Repaso/app.js
@@ -0,0 +1,14 @@
+alert('¡Bienvenida y bienvenido a nuestro sitio web!')
+let nombre = 'Luna'
+let edad = 25;
+const numeroDeVentas = 50;
+const saldoDisponible = 1000;
+alert('¡Error! Completa todos los campos')
+const mensajeDeError = '¡Error! Completa todos los campos'
+alert(mensajeDeError)
+
+nombre = prompt('Ingrese un nombre de usuario')
+edad = prompt('Ingrese su edad');
+if (edad >= 18) {
+  alert('puedes obtener tu licencia de conducir')
+}

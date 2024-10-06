5722934d372c55636ffce43e87bf8470d533f9ff
jpenagos32 <jpenagosdev@gmail.com>
2024-10-05 20:21:04 -0500

    Escritura de archivos

--- Java/POO/src/com/julianpenagos/sceenmatch/principal/PrincipalConBusqueda.java
+++ Java/POO/src/com/julianpenagos/sceenmatch/principal/PrincipalConBusqueda.java
@@ -7,6 +7,7 @@ import com.julianpenagos.sceenmatch.excepcion.ErrorEnConversionDeDuracionExcepti
 import com.julianpenagos.sceenmatch.modelos.Titulo;
 import com.julianpenagos.sceenmatch.modelos.TituloOMDB;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
@@ -14,54 +15,69 @@ import java.net.URLEncoder;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class PrincipalConBusqueda {
   public static void main(String[] args) throws IOException, InterruptedException {
 
     Scanner scan = new Scanner(System.in);
+    List<Titulo> titulos = new ArrayList<>();
 
-    System.out.println("Escriba el nombre de una pelicula: ");
-    var busqueda = scan.nextLine();
+    Gson gson = new GsonBuilder()
+      .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+      .setPrettyPrinting()
+      .create();
 
-    String direccion = "https://www.omdbapi.com/?t=" + encodeUrl(busqueda) + "&apikey=1e57290d";
+    while (true) {
+
+      System.out.println("Escriba el nombre de una pelicula: ");
+      var busqueda = scan.nextLine();
+
+      if (busqueda.equalsIgnoreCase("salir")) {
+        break;
+      }
+
+      String direccion = "https://www.omdbapi.com/?t=" + encodeUrl(busqueda) + "&apikey=1e57290d";
+
+      try {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+          .uri(URI.create(direccion))
+          .build();
+        HttpResponse<String> response = client
+          .send(request, HttpResponse.BodyHandlers.ofString());
+
+        String json = response.body();
+        System.out.println(json);
+
+        TituloOMDB miTituloOMDB = gson.fromJson(json, TituloOMDB.class);
+        System.out.println(miTituloOMDB);
+
+        Titulo miTitulo = new Titulo(miTituloOMDB);
+        System.out.println("Titulo ya convertido: " + miTitulo);
+
+        titulos.add(miTitulo);
+
+      } catch (NumberFormatException e) {
+        System.out.println("Ocurrio un error");
+        System.out.println(e.getMessage());
+      } catch (IllegalArgumentException e) {
+        System.out.println("Error en la URI, verifique la dirección");
+      } catch (ErrorEnConversionDeDuracionException e) {
+        System.out.println(e.getMessage());
+      }
 
 
-    try {
-
-      HttpClient client = HttpClient.newHttpClient();
-
-      HttpRequest request = HttpRequest.newBuilder()
-        .uri(URI.create(direccion))
-        .build();
-
-      HttpResponse<String> response = client
-        .send(request, HttpResponse.BodyHandlers.ofString());
-
-      String json = response.body();
-      System.out.println(json);
-
-      Gson gson = new GsonBuilder()
-        .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
-        .create();
-
-      TituloOMDB miTituloOMDB = gson.fromJson(json, TituloOMDB.class);
-      System.out.println(miTituloOMDB);
-
-      Titulo miTitulo = new Titulo(miTituloOMDB);
-      System.out.println("Titulo ya convertido: " + miTitulo);
-    } catch (NumberFormatException e) {
-      System.out.println("Ocurrio un error");
-      System.out.println(e.getMessage());
-    } catch (IllegalArgumentException e){
-      System.out.println("Error en la URI, verifique la dirección");
-    } catch (ErrorEnConversionDeDuracionException e){
-      System.out.println(e.getMessage());
     }
 
-    System.out.println("Finalizó la ejecución del programa");
+    FileWriter escritura = new FileWriter("titulos.json");
+    escritura.write(gson.toJson(titulos));
+    escritura.close();
 
+    System.out.println("Finalizó la ejecución del programa");
   }
 
   private static String encodeUrl(String url) {

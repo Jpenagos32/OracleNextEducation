4e2ee481f62c613c814a201c5e1c1806d0f12182
jpenagos32 <jpenagosdev@gmail.com>
2024-11-09 19:41:19 -0500

    Agregando JPA y conexion de datos al proyecto

--- SpringFramework/screenmatch_JPA/src/main/java/com/aluracursos/screenmatch/model/Categoria.java
+++ SpringFramework/screenmatch_JPA/src/main/java/com/aluracursos/screenmatch/model/Categoria.java
@@ -1,24 +1,24 @@
 package com.aluracursos.screenmatch.model;
 
 public enum Categoria {
-  ACCION("Action"),
-  ROMANCE("Romance"),
-  COMEDIA("Comedy"),
-  DRAMA("Drama"),
-  CRIMEN("Crime");
+    ACCION("Action"),
+    ROMANCE("Romance"),
+    COMEDIA("Comedy"),
+    DRAMA("Drama"),
+    CRIMEN("Crime");
 
-  private String categoriaOmdb;
-  Categoria (String categoriaOmdb){
-    this.categoriaOmdb = categoriaOmdb;
-  }
-
-  public static Categoria fromString(String text) {
-    for (Categoria categoria : Categoria.values()) {
-      if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
-        return categoria;
-      }
+    private String categoriaOmdb;
+    Categoria (String categoriaOmdb){
+        this.categoriaOmdb = categoriaOmdb;
     }
-    throw new IllegalArgumentException("Ninguna categoría encontrada: " + text);
-  }
+
+    public static Categoria fromString(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Ninguna categoria encontrada: " + text);
+    }
 
 }
--- SpringFramework/screenmatch_JPA/src/main/java/com/aluracursos/screenmatch/model/Serie.java
+++ SpringFramework/screenmatch_JPA/src/main/java/com/aluracursos/screenmatch/model/Serie.java
@@ -1,38 +1,61 @@
 package com.aluracursos.screenmatch.model;
 
-//import com.aluracursos.screenmatch.service.ConsultaChatGPT;
+import com.aluracursos.screenmatch.service.ConsultaChatGPT;
 import com.fasterxml.jackson.annotation.JsonAlias;
+import jakarta.persistence.*;
+import jdk.jfr.Enabled;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.OptionalDouble;
 
+@Entity
+@Table(name = "series")
 public class Serie {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  @Column(unique = true)
   private String titulo;
   private Integer totalTemporadas;
   private Double evaluacion;
   private String poster;
+  @Enumerated(EnumType.STRING)
   private Categoria genero;
   private String actores;
   private String sinopsis;
+  @Transient
+  private List<Episodio> episodios = new ArrayList<>();
 
-  public Serie(DatosSerie datosSerie){
+  public Serie(){}
+
+  public Serie(DatosSerie datosSerie) {
     this.titulo = datosSerie.titulo();
     this.totalTemporadas = datosSerie.totalTemporadas();
     this.evaluacion = OptionalDouble.of(Double.valueOf(datosSerie.evaluacion())).orElse(0);
     this.poster = datosSerie.poster();
     this.genero = Categoria.fromString(datosSerie.genero().split(",")[0].trim());
     this.actores = datosSerie.actores();
-    this.sinopsis = datosSerie.sinopsis();
+    this.sinopsis = datosSerie.sinopsis().trim();
   }
 
   @Override
   public String toString() {
-    return  "genero=" + genero +
+    return "genero=" + genero +
       "titulo='" + titulo + '\'' +
       ", totalTemporadas=" + totalTemporadas +
       ", evaluacion=" + evaluacion +
       ", poster='" + poster + '\'' +
       ", actores='" + actores + '\'' +
       ", sinopsis='" + sinopsis + '\'';
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
   }
 
   public String getTitulo() {
@@ -89,5 +112,13 @@ public class Serie {
 
   public void setSinopsis(String sinopsis) {
     this.sinopsis = sinopsis;
+  }
+
+  public List<Episodio> getEpisodios() {
+    return episodios;
+  }
+
+  public void setEpisodios(List<Episodio> episodios) {
+    this.episodios = episodios;
   }
 }
